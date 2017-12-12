b268977e3dedcb0f75f3f0d5ff766ac79f95ddae
Mateus Craveiro <mccraveiro@gmail.com>
2017-12-12 13:19:31 -0200

    add ramda dependency

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const moment = require('moment')
-
-const isPlaceholder = x =>
-  x != null &&
-  typeof x === 'object' &&
-  x['@@functional/placeholder'] === true
-
-const __ = { '@@functional/placeholder': true }
-
-const curry = fn => (...args) => {
-  const data = args.slice(-1)[0]
-
-  if (isPlaceholder(data)) {
-    return fn.bind(null, ...args.slice(0, -1))
-  }
-
-  return fn(...args)
-}
+const { __, curryN } = require('ramda')
 
 const now = () => moment()
 const parseISO = data => moment(data)
 const parseUnix = data => moment.unix(data)
-const parse = curry((format, data) => moment(data, format))
-const format = curry((config, data) => data.format(config))
-const add = curry((amount, key, data) => data.clone().add(amount, key))
-const subtract = curry((amount, key, data) => data.clone().subtract(amount, key))
+const parse = curryN(2, (format, data) => moment(data, format))
+const format = curryN(2, (config, data) => data.format(config))
+const add = curryN(3, (amount, key, data) => data.clone().add(amount, key))
+const subtract = curryN(3, (amount, key, data) => data.clone().subtract(amount, key))
 const getMilliseconds = data => data.milliseconds()
 const getSeconds = data => data.seconds()
 
