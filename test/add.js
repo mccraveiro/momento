fc81d98410a58ccdb2d016a1be566973228d9b1f
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-12-18 11:10:24 -0200

    change order of parameters of add and subtract
    Change the order of parameters of the add and subtract methods. The type
    (days, weeks, months) is more generic and if it comes early on the
    parameter order it will allow for a better function build up, like this
    example:
    
    ```javascript
    const addDays = add('days')
    const addWeek = addDays(7)
    const date = addWeek(now())
    ```

--- test/add.js
+++ test/add.js
@@ -3,19 +3,19 @@ import { isMoment } from 'moment'
 import { __, add, now, parseISO } from '../index'
 
 test('add should return a moment', t => {
-  t.true(isMoment(add(2, 'days', now())))
+  t.true(isMoment(add('days', 2, now())))
 })
 
 test('add should set date', t => {
-  t.true(add(2, 'days', parseISO('1995-12-25')).isSame('1995-12-27'))
+  t.true(add('days', 2, parseISO('1995-12-25')).isSame('1995-12-27'))
 })
 
 test('add should accept a placeholder on data argument', t => {
-  t.true(add(2, 'days', __)(parseISO('1995-12-25')).isSame('1995-12-27'))
+  t.true(add('days', 2, __)(parseISO('1995-12-25')).isSame('1995-12-27'))
 })
 
 test('add should not mutate data', t => {
   const data = parseISO('1995-12-25')
-  add(2, 'days', data)
+  add('days', 2, data)
   t.true(data.isSame('1995-12-25'))
 })
--- test/subtract.js
+++ test/subtract.js
@@ -3,19 +3,19 @@ import { isMoment } from 'moment'
 import { __, subtract, now, parseISO } from '../index'
 
 test('subtract should return a moment', t => {
-  t.true(isMoment(subtract(2, 'days', now())))
+  t.true(isMoment(subtract('days', 2, now())))
 })
 
 test('subtract should set date', t => {
-  t.true(subtract(2, 'days', parseISO('1995-12-25')).isSame('1995-12-23'))
+  t.true(subtract('days', 2, parseISO('1995-12-25')).isSame('1995-12-23'))
 })
 
 test('subtract should accept a placeholder on data argument', t => {
-  t.true(subtract(2, 'days', __)(parseISO('1995-12-25')).isSame('1995-12-23'))
+  t.true(subtract('days', 2, __)(parseISO('1995-12-25')).isSame('1995-12-23'))
 })
 
 test('subtract should not mutate data', t => {
   const data = parseISO('1995-12-25')
-  subtract(2, 'days', data)
+  subtract('days', 2, data)
   t.true(data.isSame('1995-12-25'))
 })
